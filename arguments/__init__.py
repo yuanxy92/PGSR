cfcec9d2062f7ef5d52bd7d19c2347295388a4d4
Hunter <yuanxy92@gmail.com>
2024-09-14 10:18:43 +0800

    add shell to automatically process all the data

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -76,11 +76,11 @@ class PipelineParams(ParamGroup):
 
 class OptimizationParams(ParamGroup):
     def __init__(self, parser):
-        self.iterations = 3000
+        self.iterations = 5000
         self.position_lr_init = 0.00016
         self.position_lr_final = 0.0000016
         self.position_lr_delay_mult = 0.01
-        self.position_lr_max_steps = 3000
+        self.position_lr_max_steps = 5000
         self.feature_lr = 0.0025
         self.opacity_lr = 0.05
         self.scaling_lr = 0.005
