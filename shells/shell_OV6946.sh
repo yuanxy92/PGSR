cd4feb5059d152e38639ba7d82a7da1367f45c00
Hunter <yuanxy92@gmail.com>
2024-10-07 17:07:13 +0800

    Update shell_OV6946.sh

--- shells/shell_OV6946.sh
+++ shells/shell_OV6946.sh
@@ -27,17 +27,62 @@
 #         --max_depth 60.0 --voxel_size 0.05 --iteration 2000
 # done
 
-for i in $(seq -w 000 018);
+# for i in $(seq -w 000 018);
+# do
+#     python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_recon/colmap \
+#         -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_recon/pgsr_depth_normal \
+#         --max_abs_split_points 0 --opacity_cull_threshold 0.05 
+#     python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_recon/pgsr_depth_normal \
+#         --max_depth 60.0 --voxel_size 0.05 --iteration 2000
+
+#     python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_sr_recon/colmap \
+#         -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_sr_recon/pgsr_depth_normal \
+#         --max_abs_split_points 0 --opacity_cull_threshold 0.05 
+#     python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_sr_recon/pgsr_depth_normal \
+#         --max_depth 60.0 --voxel_size 0.05 --iteration 2000
+# done
+
+for i in $(seq -w 000 004);
 do
-    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_recon/colmap \
-        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_recon/pgsr_depth_normal \
+    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_2/${i}_recon/colmap \
+        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_2/${i}_recon/pgsr_depth_normal \
         --max_abs_split_points 0 --opacity_cull_threshold 0.05 
-    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_recon/pgsr_depth_normal \
+    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_2/${i}_recon/pgsr_depth_normal \
         --max_depth 60.0 --voxel_size 0.05 --iteration 2000
 
-    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_sr_recon/colmap \
-        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_sr_recon/pgsr_depth_normal \
+    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_2/${i}_sr_recon/colmap \
+        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_2/${i}_sr_recon/pgsr_depth_normal \
         --max_abs_split_points 0 --opacity_cull_threshold 0.05 
-    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241003_1/${i}_sr_recon/pgsr_depth_normal \
+    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_2/${i}_sr_recon/pgsr_depth_normal \
+        --max_depth 60.0 --voxel_size 0.05 --iteration 2000
+done
+
+for i in $(seq -w 000 005);
+do
+    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_3/${i}_recon/colmap \
+        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_3/${i}_recon/pgsr_depth_normal \
+        --max_abs_split_points 0 --opacity_cull_threshold 0.05 
+    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_3/${i}_recon/pgsr_depth_normal \
+        --max_depth 60.0 --voxel_size 0.05 --iteration 2000
+
+    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_3/${i}_sr_recon/colmap \
+        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_3/${i}_sr_recon/pgsr_depth_normal \
+        --max_abs_split_points 0 --opacity_cull_threshold 0.05 
+    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_3/${i}_sr_recon/pgsr_depth_normal \
+        --max_depth 60.0 --voxel_size 0.05 --iteration 2000
+done
+
+for i in $(seq -w 000 012);
+do
+    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_4/${i}_recon/colmap \
+        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_4/${i}_recon/pgsr_depth_normal \
+        --max_abs_split_points 0 --opacity_cull_threshold 0.05 
+    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_4/${i}_recon/pgsr_depth_normal \
+        --max_depth 60.0 --voxel_size 0.05 --iteration 2000
+
+    python train.py -s /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_4/${i}_sr_recon/colmap \
+        -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_4/${i}_sr_recon/pgsr_depth_normal \
+        --max_abs_split_points 0 --opacity_cull_threshold 0.05 
+    python render.py -m /home/luvision/project/Code/data/Aurora/Fig_4/Softgripper_demo/capture_20241004_4/${i}_sr_recon/pgsr_depth_normal \
         --max_depth 60.0 --voxel_size 0.05 --iteration 2000
 done
